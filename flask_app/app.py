07f7531f9c303644a203f488109eaede5acf2d0d
abizerjafferjee <abizerjafferjee@gmail.com>
2019-11-21 19:33:54 -0500

    fixing app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,7 +7,6 @@ from flask_cors import CORS
 
 # internal imports
 from flask_app.config import Config
-from flask_app.views import *
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +17,7 @@ ma = Marshmallow(app)
 # enable CORS
 cors = CORS(app)
 
+from flask_app.views import *
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
